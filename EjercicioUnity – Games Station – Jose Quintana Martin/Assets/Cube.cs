7c2da6ecf48f84a7f6bf2d79afb336aa6dc975aa
José Quintana Martín <jquintanamartin@hotmail.com>
2022-04-12 17:21:16 -0300

    When cubes collide they get destroyed

---
+++ EjercicioUnity – Games Station – Jose Quintana Martin/Assets/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Cube : MonoBehaviour
+{
+    [SerializeField] Vector3 force;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(force);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
