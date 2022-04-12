389e7f1415a7cb96cb66b4d1c4d4cf494912987d
José Quintana Martín <jquintanamartin@hotmail.com>
2022-04-12 17:08:28 -0300

    Cube and sphere spawns when you press their respective buttons

---
+++ EjercicioUnity – Games Station – Jose Quintana Martin/Assets/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject cubePrefab;
+    [SerializeField] GameObject spherePrefab;
+
+    public void SpawnCube() {
+        Instantiate(cubePrefab, transform.position, Quaternion.identity);
+    }
+
+    public void SpawnSphere()
+    {
+        Instantiate(spherePrefab, transform.position, Quaternion.identity);
+    }
+}
